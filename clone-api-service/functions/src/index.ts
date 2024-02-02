797ace34e80792cc1d3e7c2ec2b541553193a4cb
Danyal <112341859+theonlynanu@users.noreply.github.com>
2024-02-02 11:21:52 -0500

    Add getVideos function to firebase API

--- clone-api-service/functions/src/index.ts
+++ clone-api-service/functions/src/index.ts
@@ -11,6 +11,7 @@ const firestore = new Firestore();
 const storage = new Storage();
 
 const rawVideoBucket = "theonlynanu-raw-videos";
+const videoCollectionId = "videos";
 
 export const createUser = functions.auth.user().onCreate((user) => {
   const userInfo = {
@@ -50,3 +51,11 @@ export const generateUploadUrl = onCall(
     return { url, fileName };
   }
 );
+
+export const getVideos = onCall({ maxInstances: 1 }, async () => {
+  const snapshot = await firestore
+    .collection(videoCollectionId)
+    .limit(10)
+    .get();
+  return snapshot.docs.map((doc) => doc.data());
+});
